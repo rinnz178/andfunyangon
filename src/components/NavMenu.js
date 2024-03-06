27f7a29f00b017762fbe5f618c3c4971e7cb8a6d
Linn Nanda Aung <95839418+rinnz178@users.noreply.github.com>
2024-03-06 15:24:08 +0630

    Update NavMenu.js

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -41,7 +41,7 @@ const NavMenu = () => {
               <Offcanvas.Body >
                 <Nav className="justify-content-end flex-grow-1 pe-3" >
                   <Nav.Link href="#action1" className="px-3" style={{color:'white'}}>
-                    <Link to="/">
+                    <Link to="/andfunyangon">
                       Home
                     </Link>
                   </Nav.Link >
